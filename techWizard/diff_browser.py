fd3a00341f58cb252d9ad5eed1a5147a7822028c
Tanmay Patil <t-patil@ti.com>
2022-07-15 09:59:03 +0530

    Builder working
    Search Engine Done
    Diff top level and detailed remaining
    
    Signed-off-by: Tanmay Patil <t-patil@ti.com>

---
+++ techWizard/diff_browser.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import os
+
+
+# versioned_components = {'rtos':['pdk_jacinto'],'linux':['linux','u-boot']}
+versioned_components = ['pdk_jacinto', 'linux', 'u-boot', 'cg_xml', 'dsplib_c66', 'mathlib_c66', 'mmalib', 'ti-cgt-armllvm', 'ti-cgt-c6000', 'ti-cgt-c7000', 'tidl_j7', 'uia', 'xdais']
+g_back_map = [{},{}]
+
+def short_dir_names(real_list,idx):
+    global g_back_map
+    new_list = []
+    for real_item in real_list:
+        changed=False
+        for short_item in versioned_components:
+            if short_item in real_item:
+                new_list.append(short_item)
+                g_back_map[idx][short_item] = real_item
+                changed=True
+                break
+        if not changed:
+            new_list.append(real_item)
+    return new_list
+
+
+# inputs:
+#     path1     if exists, must be directory
+#     path2     if exists, must be directory
+# outputs:
+#     characterized union of contents with change status : 'r' 'a' 'm' 'u'
+def browse_diff_dir(path1,path2,relative_path):
+    lst1,lst2 = [],[]
+    if (os.path.exists(path1)):
+        lst1 = sorted(os.listdir(path1))
+    if (os.path.exists(path2)):
+        lst2 = sorted(os.listdir(path2))
+
+    lst1 = short_dir_names(lst1,0)
+    lst2 = short_dir_names(lst2,1)
+
+    union_list = sorted(list(set(lst1) | set(lst2)))
+    intersection_list = sorted(list(set(lst1) & set(lst2)))
+    lst1 = [item for item in lst1 if item not in intersection_list]
+    lst2 = [item for item in lst2 if item not in intersection_list]
+
+    diff_map = []
+
+    changes_str = get_diff_patch(path1,path2,"-qrbBX excludefiles.txt")
+
+    for item in union_list:
+        if item.startswith('.') :
+            continue
+        if item in versioned_components:
+            complete_path1 = path1 + '/' + g_back_map[0][item]
+            complete_path2 = path2 + '/' + g_back_map[1][item]
+        else:
+            complete_path1 = path1 + '/' + item
+            complete_path2 = path2 + '/' + item
+        item_relative_path = relative_path + '/' + item
+
+        itemtype = ''
+        if os.path.isdir(complete_path1) or os.path.isdir(complete_path2):
+            itemtype = 'd'
+        elif os.path.isfile(complete_path1) or os.path.isfile(complete_path2):
+            itemtype = 'f'
+
+        if item in lst1:
+            status = '#b42626'
+
+        if item in lst2:
+            status = '#5dd159'
+
+        if item in intersection_list:
+            if item in changes_str:
+                status = '#b1c33e'
+            else:
+                status = '#1f1f1f'
+
+        diff_map.append((item, itemtype , status, complete_path1, complete_path2, item_relative_path))
+
+    return diff_map
+
+#
+# inputs:
+#     path1     if exists, must be a file
+#     path2     if exists, must be a file
+# outputs:
+#     string that has the diff content
+def get_diff_patch(path1,path2, opt = ''):
+    command = "diff " + opt + " " + path1 + " " + path2
+    return os.popen(command).read()
+
+
+
+def browse_diff(complete_path1, complete_path2 ,relative_path, my_type):
+    while True:
+        print("\nYou are at: ." + relative_path)
+        if my_type == 'd':
+            diff_map = browse_diff_dir(complete_path1, complete_path2, relative_path)
+            for item in diff_map:
+                print(str(diff_map[item]))
+
+            goto = input("\ngo to: ")
+
+            if goto == 'x':
+                break
+            elif goto == 'p':
+                print(get_diff_patch(complete_path1,complete_path2,"-qrbBX excludefiles.txt"))
+            # if goto == '^':
+                # back one folder
+            elif goto not in diff_map:
+                print("{" + goto + "} Not found!\n")
+            else:
+                # from here...
+                complete_path1 = diff_map[goto][3]
+                complete_path2 = diff_map[goto][4]
+                relative_path  = diff_map[goto][5]
+                my_type        = diff_map[goto][1]
+        elif my_type == 'f':
+            file_contents = get_diff_patch(complete_path1, complete_path2, "-NbBu")
+            print(file_contents)
+            break
+
+
+
+if __name__ == "__main__":
+    # MAIN STARTS HERE
+    SOC="j721e"
+    OS="rtos"
+    VERSION1="08_01_00_13"
+    VERSION2="08_02_00_05"
+
+
+    base_path = '/home/tanmay/inspire/sdk/'
+    SDK1_PATH = base_path + SOC + '/'+ OS + '/' + VERSION1
+    SDK2_PATH = base_path + SOC + '/'+ OS + '/' + VERSION2
+
+    relative_path = ''
+    relative_path = ''
+    # relative_path += '/' + 'vision_apps'
+    # relative_path += '/' + 'platform'
+    # relative_path += '/' + 'apps/basic_demos/app_multi_cam/main.c'
+    my_type = 'd'
+    # my_type = 'f'
+    complete_path1 = SDK1_PATH + relative_path
+    complete_path2 = SDK2_PATH + relative_path
+    
+    browse_diff(complete_path1, complete_path2 ,relative_path, my_type)
--- techWizard/round2/views.py
+++ techWizard/round2/views.py
@@ -3,8 +3,13 @@ from django.views.generic import TemplateView
 import os
 import json
 from django.http import HttpResponse
+from diff_browser import *
 
+gbase_path_perm = "/home/tanmay/inspire/sdk/"
 gbase_path = "/home/tanmay/inspire/sdk/"
+global_base1 = gbase_path_perm
+global_base2 = gbase_path_perm
+
 
 SS_IS_UNKNOWN=0
 SS_IS_IF=1
@@ -120,16 +125,22 @@ class Test(TemplateView):
 class Test2(TemplateView):
     def get(self, request, path_name=''):
         fileText = ""
+        print(gbase_path)
         if path_name:
             base_path = gbase_path + path_name + "/"
         else:
-            base_path = gbase_path           
+            base_path = gbase_path_perm           
         if (os.path.isdir(base_path) == False):
             tl=[i for i in base_path.split('/') if i]
             base_path = '/' + '/'.join(tl[:-1]) + '/'
             scriptFile = open(base_path + tl[-1], "r")
+            num_lines = sum(1 for line in scriptFile)
+            scriptFile = open(base_path + tl[-1], "r")
+            digits = len(str(num_lines))
+            i = 1
             for fullLine in scriptFile:
-                fileText = str(fileText) + str(fullLine)
+                fileText = str(fileText) + str(i).ljust(digits+1, ' ') + str(fullLine)
+                i = i + 1
         dir_list = os.listdir(base_path)
         dir_tuple = list()
         for item in dir_list:
@@ -147,14 +158,16 @@ class Test2(TemplateView):
 
 
 class Test3(TemplateView):
-    def get(self, request, path_name):
-        base_path = gbase_path + path_name
+    def get(self, request, **kwargs):
         fileText = ""
-        if (os.path.isfile(base_path) == True):
-            scriptFile = open(base_path, "r")
-            for fullLine in scriptFile:
-                fileText = str(fileText) + str(fullLine)
-        return render(request, 'browser.html', {'fileTest' : fileText})
+        sdktype = request.GET["sdktype"]
+        soc = request.GET["soc"] 
+        sdk = request.GET["sdk"]
+        global gbase_path
+        gbase_path = gbase_path_perm + soc + '/' + sdktype + '/' + sdk + '/'
+        print(gbase_path)
+        dir_list = ["Success"]
+        return HttpResponse(json.dumps(dir_list))
 
 class Test4(TemplateView):
     def get(self, request, **kwargs):
@@ -166,8 +179,9 @@ class Test5(TemplateView):
     def get(self, request, **kwargs):
         sdktype = request.GET["sdktype"]
         soc = request.GET["soc"] 
-        folderpath = gbase_path+soc+"/"+sdktype     
+        folderpath = gbase_path_perm+soc+"/"+sdktype     
         dir_list = os.listdir(folderpath)
+        print("test 5 grtting called")
 
         return HttpResponse(json.dumps(dir_list))
 
@@ -184,10 +198,10 @@ class Test6(TemplateView):
         folderpath=""
         make = ""
         if sdktype=="linux":
-            folderpath = gbase_path+soc+"/"+sdktype+"/"+sdk 
+            folderpath = gbase_path_perm+soc+"/"+sdktype+"/"+sdk 
             make = "make -s "+appname
         else:
-            folderpath = gbase_path+soc+"/"+sdktype+"/"+sdk+"/pdk*/packages/ti/build"
+            folderpath = gbase_path_perm+soc+"/"+sdktype+"/"+sdk+"/pdk*/packages/ti/build"
             make = "make -s CORE="+core+" -C " + folderpath + " BUILD_PROFILE="+ build + " " + appname + " > output.txt"
         print(folderpath + " " + make)
         # print(1)
@@ -197,7 +211,7 @@ class Test6(TemplateView):
         fileText = ""
         scriptFile = open('output.txt', "r")
         for fullLine in scriptFile:
-            fileText = str(fileText) + str(fullLine)
+            fileText = str(fileText) + "" + str(fullLine)
         
         print(fileText)
         temp = render(request, 'builder.html', {'fileText': fileText})
@@ -217,78 +231,134 @@ class Test6(TemplateView):
 class Test7(TemplateView):
     def post(self, request, path_name=""):
         if request.method == "POST":
-            searchtext = request.POST['searchtext']
+            searchtext = request.POST.get('searchtext', False)
         base_path = gbase_path
         dir_list = os.listdir(base_path)
         dir_tuple = list()
+
+        if not searchtext:
+            isSearch = 0
+        else : 
+            isSearch = 1
+
         for item in dir_list:
             if item.startswith('.'):
                 continue
             complete_path = base_path+"/"+item
             ref_path = "/browser/" + os.path.relpath(complete_path, gbase_path)
             if os.path.isdir(complete_path):
-                dir_tuple.append((item, 'd', ref_path, 1))
+                dir_tuple.append((item, 'd', ref_path, isSearch))
             else:
-                dir_tuple.append((item, 'f', ref_path, 1))
-                
-        search_base_dir = '/home/tanmay/inspire/sdk/j721e/rtos/08_01_00_13/'
-        os.chdir(search_base_dir)
-        search_rel_path = os.path.relpath(search_base_dir, gbase_path) + '/'
-        mylst = get_search_results_standard(searchtext)
-        srch_res = []
-        tt = []
+                dir_tuple.append((item, 'f', ref_path, isSearch))
+            srch_res = []
 
-        if mylst[0] == SS_IS_UNKNOWN :
-            srch_res.append((0, "Cannot find any reference for " + searchtext))
-        
-        if mylst[0] == SS_IS_IF or mylst[0] == SS_IS_STRUCT or mylst[0] == SS_IS_MACRO:
-            ts = 'file' if mylst[0] == SS_IS_IF else 'Structure' if mylst[0] == SS_IS_STRUCT else 'MACRO'
-            srch_res.append((1, searchtext + " is a " + ts + "."))
-            srch_res.append((1, "Defination(s) of " + searchtext + " :"))
-            for item in mylst[2] :
-                tt.append((item[0]+'@'+str(item[1]), '/browser/' + search_rel_path + item[0], ''))
-            srch_res.append((0, tt))
-            tt=[]
-            srch_res.append((1, "References for " + searchtext + " :"))
-            for item in mylst[3] :
-                tt.append((item[0]+'@'+str(item[1]), '/browser/' + search_rel_path + item[0], ''))
-            srch_res.append((0, tt))
-            tt=[]
+        if isSearch : 
+            search_base_dir = '/home/tanmay/inspire/sdk/j721e/rtos/08_01_00_13/'
+            os.chdir(search_base_dir)
+            search_rel_path = os.path.relpath(search_base_dir, gbase_path) + '/'
+            mylst = get_search_results_standard(searchtext)
+            tt = []
 
-        if mylst[0] == SS_IS_FUNC :
-            srch_res.append((1, searchtext + " is a Function."))
-            srch_res.append((1, "Defination(s) of " + searchtext + " :"))
-            for item in mylst[2] :
-                tt.append((item[0]+'@'+str(item[1]), '/browser/' + search_rel_path + item[0], ' ( ' + item[2] + ' )'))
-            srch_res.append((0, tt))
-            tt=[]
-            srch_res.append((1, "Funtions called in " + searchtext + " :"))
-            for item in mylst[3] :
-                tt.append((item[0]+'@'+str(item[2]), '/browser/' + search_rel_path + item[0], ' ( ' + item[1] + ' )'))
-            srch_res.append((0, tt))
-            tt=[]
-            srch_res.append((1, "Funtions calling " + searchtext + " :"))
-            for item in mylst[4] :
-                tt.append((item[0]+'@'+str(item[2]), '/browser/' + search_rel_path + item[0], ' ( ' + item[1] + ' )'))
-            srch_res.append((0, tt))
-            tt=[]
+            if mylst[0] == SS_IS_UNKNOWN :
+                srch_res.append((0, "Cannot find any reference for " + searchtext))
+            
+            if mylst[0] == SS_IS_IF or mylst[0] == SS_IS_STRUCT or mylst[0] == SS_IS_MACRO:
+                ts = 'file' if mylst[0] == SS_IS_IF else 'Structure' if mylst[0] == SS_IS_STRUCT else 'MACRO'
+                srch_res.append((1, searchtext + " is a " + ts + "."))
+                srch_res.append((1, "Defination(s) of " + searchtext + " :"))
+                for item in mylst[2] :
+                    tt.append((item[0]+'@'+str(item[1]), '/browser/' + search_rel_path + item[0], ''))
+                srch_res.append((0, tt))
+                tt=[]
+                srch_res.append((1, "References for " + searchtext + " :"))
+                for item in mylst[3] :
+                    tt.append((item[0]+'@'+str(item[1]), '/browser/' + search_rel_path + item[0], ''))
+                srch_res.append((0, tt))
+                tt=[]
 
-        if mylst[0] == SS_IS_VAR :
-            srch_res.append((1, searchtext + " is a Variable."))
-            srch_res.append((1, "References of " + searchtext + " :"))
-            for item in mylst[2] :
-                tt.append((item[0]+'@'+str(item[2]), '/browser/' + search_rel_path + item[0], ' ( ' + item[1] + ' )'))
-            srch_res.append((0, tt))
-            tt=[]
+            if mylst[0] == SS_IS_FUNC :
+                srch_res.append((1, searchtext + " is a Function."))
+                srch_res.append((1, "Defination(s) of " + searchtext + " :"))
+                for item in mylst[2] :
+                    tt.append((item[0]+'@'+str(item[1]), '/browser/' + search_rel_path + item[0], ' ( ' + item[2] + ' )'))
+                srch_res.append((0, tt))
+                tt=[]
+                srch_res.append((1, "Funtions called in " + searchtext + " :"))
+                for item in mylst[3] :
+                    tt.append((item[0]+'@'+str(item[2]), '/browser/' + search_rel_path + item[0], ' ( ' + item[1] + ' )'))
+                srch_res.append((0, tt))
+                tt=[]
+                srch_res.append((1, "Funtions calling " + searchtext + " :"))
+                for item in mylst[4] :
+                    tt.append((item[0]+'@'+str(item[2]), '/browser/' + search_rel_path + item[0], ' ( ' + item[1] + ' )'))
+                srch_res.append((0, tt))
+                tt=[]
 
-        isSearch = 1            
+            if mylst[0] == SS_IS_VAR :
+                srch_res.append((1, searchtext + " is a Variable."))
+                srch_res.append((1, "References of " + searchtext + " :"))
+                for item in mylst[2] :
+                    tt.append((item[0]+'@'+str(item[2]), '/browser/' + search_rel_path + item[0], ' ( ' + item[1] + ' )'))
+                srch_res.append((0, tt))
+                tt=[]
 
-        return render(request, 'browser.html', {'dir_tuple' : dir_tuple, 'fileTest' : str(mylst) , 'isSearch' : isSearch , 'srch_res' : srch_res})
+
+        return render(request, 'browser.html', {'dir_tuple' : dir_tuple, 'fileTest' : '' , 'isSearch' : isSearch , 'srch_res' : srch_res})
 
 class Test8(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'c4key.html', context=None)
+        return render(request, 'diff.html', context=None)
 
 class Test9(TemplateView):
     def get(self, request, **kwargs):
-        return render(request, 'circuit.html', context=None)
+        sdktype = request.GET["sdktype"]
+        soc = request.GET["soc"]
+        sdk1 = request.GET["sdk1"]
+        sdk2 = request.GET["sdk2"]
+        global gbase_path1
+        global gbase_path2
+        gbase_path1 = gbase_path_perm + soc + '/' + sdktype + '/' + sdk1 + '/'
+        gbase_path2 = gbase_path_perm + soc + '/' + sdktype + '/' + sdk2 + '/'
+        print(gbase_path1, gbase_path2)
+        resp = ["Success"]
+        return HttpResponse(json.dumps(resp))
+
+    def post(self, request, **kwargs):
+        if request.method == "POST":
+            searchtext = request.POST.get('searchtext', False)
+        base_path = gbase_path1
+        lst = browse_diff_dir(gbase_path1, gbase_path2, '')
+        parent = "/"
+        return render(request, 'diff.html', {'parent' : parent, 'dir_tuple' : lst})
+
+class Test10(TemplateView):
+    def get(self, request, path_name):
+        fileText = ""
+        print(path_name)
+
+        if path_name:
+            tl = path_name.split('&')
+            if tl[1] == "abs_ref/" :
+                path1 = tl[2]
+                path2 = tl[3]
+                relpath = tl[4]
+                parent = relpath.split('/')[-1]
+        else:
+            path1 = gbase_path1
+            path2 = gbase_path2
+            parent = "/"
+        
+        lst = browse_diff_dir(path1, path2, relpath)
+
+        stats = [('.c', 23, 45, 67), ('.lds', 23, 45, 67), ('.cfg', 23, 45, 67), ('Makefiles', 23, 45, 67)]
+        pstats = [('top_lvl', 35, 67, 65)]
+
+        return render(request, 'diff.html', {'parent' : parent, 'dir_tuple' : lst, 'stats' : stats, 'pstats' : pstats})
+
+class Test11(TemplateView):
+    def get(self, request, path_name):
+        tl = path_name.split('&')
+        Textfile = get_diff_patch(tl[-2], tl[-1])
+        if not Textfile :
+            Textfile = "No changes for this file"
+        return render(request, 'display_file.html', {'Textfile' : Textfile})
